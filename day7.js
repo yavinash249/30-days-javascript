133a41f5a742f4e5f7f7e049e8e093a7f0b1e1b0
Avinash Yadav <95971529+yavinash249@users.noreply.github.com>
2024-07-21 13:36:34 +0530

    day7

---
+++ day7.js
@@ -0,0 +1,52 @@
+let Book = {
+    title : "Wishpers From A Grave",
+    author : "Tamanna Sharma",
+    year : 2024,
+    getTitleandAuthor : ()=>{
+        return`${Book.title} by ${Book.author}`
+    },
+
+    getYear : (year) =>{
+        return `This book was not published on ${Book.year} but in ${year}`
+    },
+
+    getString : function () {
+        return `Book ${this.title} by author ${this.author} was publish in year ${this.year}`
+    }
+}
+console.log(Book);
+console.log(Book.title);
+console.log(Book.author);
+console.log(Book.getTitleandAuthor());
+console.log(Book.getYear(2023));
+console.log(Book.getString());
+
+
+let library = {
+    name: "DAV Library",
+    books: [
+        {
+            name : "Economic",
+        },
+        {
+            name : "Accounts",
+        },
+        {
+            name : "Basic Maths"
+        }
+    ]
+}
+console.log(library);
+console.log(library.name);
+library.books.forEach(book =>{
+    console.log((book.name));
+})
+
+//Activity: 5
+//Object iteration
+for (const property in Book) {
+    console.log(`${property} : ${Book[property]}`);
+}
+
+console.log(Object.keys(Book).toString());
+console.log(Object.values(Book).toString());
